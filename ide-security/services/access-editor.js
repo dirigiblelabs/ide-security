81775e707f60d04e32e49dcb4bd8a9685cc97d1b
StanZGenchev <stan.z.genchev@gmail.com>
2022-11-30 11:03:55 +0200

    Migrated editors to the new view system

--- ide-security/services/access-editor.js
+++ ide-security/services/access-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,16 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getEditor = function () {
-	return {
-		"id": "access",
-		"name": "Access",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Access",
-		"link": "../ide-security/editors/access/editor.html",
-		"contentTypes": ["application/json+access"]
-	};
+const editorData = {
+	id: "access",
+	label: "Access",
+	factory: "frame",
+	region: "center",
+	link: "../ide-security/editors/access/editor.html",
+	contentTypes: ["application/json+access"]
 };
+if (typeof exports !== 'undefined') {
+	exports.getEditor = function () {
+		return editorData;
+	}
+}
--- ide-security/services/roles-editor.js
+++ ide-security/services/roles-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,16 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getEditor = function () {
-	return {
-		"id": "roles",
-		"name": "Roles",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Roles",
-		"link": "../ide-security/editors/roles/editor.html",
-		"contentTypes": ["application/json+roles"]
-	};
+const editorData = {
+	id: "roles",
+	label: "Roles",
+	factory: "frame",
+	region: "center",
+	link: "../ide-security/editors/roles/editor.html",
+	contentTypes: ["application/json+roles"]
 };
+if (typeof exports !== 'undefined') {
+	exports.getEditor = function () {
+		return editorData;
+	}
+}
